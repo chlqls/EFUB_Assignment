0333a9b540fad60f9284d3c544745ed480d1786d
chlqls <99929277+chlqls@users.noreply.github.com>
2022-05-09 13:01:49 +0900

    Add files via upload

---
+++ Week6/efub/src/test/java/efub/efub/EfubApplicationTests.java
@@ -0,0 +1,13 @@
+package efub.efub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
